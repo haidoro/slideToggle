ad59ee6b2c68471b725c9e4719436c3c905cb692
Tahara <tahara@studiom-web.net>
2018-07-01 19:49:46 +0900

    javascriptとCSS3でslideToggleを実施

--- slideToggle.js
+++ slideToggle.js
@@ -1,5 +1,15 @@
-$(function(){
-	$('dt').click(function(){
-		$('dd:not(:animated)').slideToggle('slow');
-	})
-})
+var def = document.getElementById('block-def');
+var dt = def.firstElementChild;
+var dd = def.lastElementChild;
+var flag = false;
+
+dt.addEventListener('click',myToggle);
+function myToggle(){
+	if(flag){
+		dd.classList.add('on');
+		flag = false;
+	}else{
+		dd.classList.remove('on');
+		flag = true;
+	}
+}
