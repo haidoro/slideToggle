fc47b7a7e4d7e64cda148d16c109bb6e445a527d
Tahara <tahara@studiom-web.net>
2018-07-02 14:05:35 +0900

    関数を無名関数にしてaddEventListenerに活用

--- slideToggle.js
+++ slideToggle.js
@@ -2,7 +2,6 @@ var def = document.getElementById('block-def');
 var dt = def.firstElementChild;
 var dd = def.lastElementChild;
 
-dt.addEventListener('click',myToggle);
-function myToggle(){
+dt.addEventListener('click',function(){
 		dd.classList.toggle( "on" );
-}
+})
