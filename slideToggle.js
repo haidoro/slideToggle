5c58b09655a35b24d5079048d46ea2f750c62d34
Tahara <tahara@studiom-web.net>
2018-07-02 14:02:37 +0900

    classList.toggle()を活用

--- slideToggle.js
+++ slideToggle.js
@@ -1,15 +1,8 @@
 var def = document.getElementById('block-def');
 var dt = def.firstElementChild;
 var dd = def.lastElementChild;
-var flag = false;
 
 dt.addEventListener('click',myToggle);
 function myToggle(){
-	if(flag){
-		dd.classList.add('on');
-		flag = false;
-	}else{
-		dd.classList.remove('on');
-		flag = true;
-	}
+		dd.classList.toggle( "on" );
 }
